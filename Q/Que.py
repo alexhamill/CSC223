ce50dc56b884b18c21bd0133d14d405ef31b64a5
Alex Hamill <alexander.hamill@icloud.com>
2026-02-19 08:28:26 -0500

    wokring though it

--- Q/Que.py
+++ Q/Que.py
@@ -1,8 +1,26 @@
-line = []
-with open("shoppers.txt", "r") as f:
-    line = f.readlines()
+import random
 
-for i in range(len(line)):
-    line[i] = line[i].strip()
+line = []
+store = []
+with open("shoppers.txt", "r") as f:
+    store = f.readstores()
+
+for i in range(len(store)):
+    store[i] = store[i].strip()
+
+while len(store) > 0:
+    if (len(line) >= 10):
+        leave()
+    elif (len(line) == 0):
+        enter()
+
+
+def leave():
+    print(line.pop(0), "leaves the store.")
+
+def enter():
+    cur = random.randint(0, len(store) - 1)
+    line.append(store.pop(cur))
 
 print(line)
+print(store)
