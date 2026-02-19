86a2d7ef2c8f45fcb160d91d7f3d7de923e75367
Alex Hamill <alexander.hamill@icloud.com>
2026-02-19 08:34:45 -0500

    done i think

--- Q/Que.py
+++ Q/Que.py
@@ -1,26 +1,39 @@
 import random
 
+def leave():
+    print(line.pop(0), "finishes checkout and leaves.")
+
+def enter():
+    if len(store) > 0:
+        cur = random.randint(0, len(store) - 1)
+        line.append(store.pop(cur))
+        print(line[-1], "gets in line.")
+
+
 line = []
 store = []
 with open("shoppers.txt", "r") as f:
-    store = f.readstores()
+    store = f.readlines()
 
 for i in range(len(store)):
     store[i] = store[i].strip()
 
-while len(store) > 0:
+
+while len(line) > 0 or len(store) > 0:
     if (len(line) >= 10):
         leave()
     elif (len(line) == 0):
         enter()
+    else:
+        if random.randint(0, 3) == 0:
+            leave()
+        else:
+            enter()
+
+    print(line)
+    print(store)
 
 
-def leave():
-    print(line.pop(0), "leaves the store.")
-
-def enter():
-    cur = random.randint(0, len(store) - 1)
-    line.append(store.pop(cur))
 
 print(line)
 print(store)
