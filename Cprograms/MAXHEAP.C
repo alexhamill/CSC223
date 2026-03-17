77e18e6136a8d8ef7c4b71f24640d1803ae8bf93
Alex Hamill <alexander.hamill@icloud.com>
2026-03-17 01:04:49 -0400

    remove seems to be broken

---
+++ Cprograms/MAXHEAP.C
@@ -0,0 +1,186 @@
+#include <stdio.h>
+
+#define MAX_SIZE 64
+#define NULL_NODE -1
+
+struct node {
+    int value;
+    int parent;
+    int left;
+    int right;
+};
+
+struct node storage[MAX_SIZE];
+int storage_size;
+int root;
+
+
+new_node(val, par)
+int val, par;
+{
+    int i;
+    i = storage_size++;
+    storage[i].value  = val;
+    storage[i].parent = par;
+    storage[i].left   = NULL_NODE;
+    storage[i].right  = NULL_NODE;
+    return i;
+}
+
+swap_vals(a, b)
+int a, b;
+{
+    int tmp;
+    tmp = storage[a].value;
+    storage[a].value = storage[b].value;
+    storage[b].value = tmp;
+}
+
+
+insert(val)
+int val;
+{
+    int i, par, pval;
+
+    if (storage_size >= MAX_SIZE) {
+        printf("full!!\n");
+        return;
+    }
+
+
+    par = (storage_size == 0) ? NULL_NODE : (storage_size - 1) / 2;
+    i = new_node(val, par);
+
+    if (i == 0) {
+        root = 0;
+    } else {
+        
+        if (storage[par].left == NULL_NODE)
+            storage[par].left = i;
+        else
+            storage[par].right = i;
+    }
+
+    
+    for (;;) {
+        int p;
+        p = storage[i].parent;
+        if (p == NULL_NODE) break;
+        pval = storage[p].value;
+        if (storage[i].value <= pval) break;
+        swap_vals(i, p);
+        i = p;
+    }
+
+    printf("Inserted %d.\n", val);
+}
+
+remove_leaf(i)
+int i;
+{
+    int par;
+    par = storage[i].parent;
+    if (par != NULL_NODE) {
+        if (storage[par].left == i)
+            storage[par].left = NULL_NODE;
+        else
+            storage[par].right = NULL_NODE;
+    }
+    storage_size--;
+}
+
+remove(i)
+int i;
+{
+    int c, lc, rc;
+    c = i;
+    for (;;) {
+    if (storage[c].left == NULL_NODE && storage[c].right == NULL_NODE) {
+        remove_leaf(c);
+        break;
+    }
+    lc = storage[c].left;
+    rc = storage[c].right;
+    if (storage[lc].value >= storage[rc].value || storage[c].right == NULL_NODE) {
+        swap_vals(c, storage[c].left);
+        c = storage[c].left;
+    } else {
+        swap_vals(c, storage[c].right);
+        c = storage[c].right;
+    }
+}
+}
+
+
+print_heap()
+{
+    int i;
+    if (storage_size == 0) {
+        printf("Heap is empty.\n");
+        return;
+    }
+    printf("Heap (%d nodes):\n", storage_size);
+    printf(" index  val  par  L   R\n");
+    printf("  ---  ---  ---  ---  ---\n");
+    for (i = 0; i < storage_size; i++) {
+        printf(" %3d  %3d  %3d  %3d  %3d\n",
+            i,
+            storage[i].value,
+            storage[i].parent,
+            storage[i].left,
+            storage[i].right);
+    }
+    printf("   max=%d\n", storage[0].value);
+}
+
+
+
+main()
+{
+    char cmd;
+    int  val, n;
+
+    storage_size = 0;
+    root      = NULL_NODE;
+
+    printf("Max Heap (struct nodes)\n");
+    printf("  i <num>  insert\n");
+    printf("  r <num>  remove \n");
+    printf("  p        print\n");
+    printf("  q        quit\n\n");
+
+    for (;;) {
+        printf("> ");
+        cmd = getchar();
+        switch (cmd) {
+        case 'i':
+            n = scanf("%d", &val);
+            if (n < 1)
+                printf("Usage: i <number>\n");
+            else
+                insert(val);
+            break;
+        case 'r':
+            n = scanf("%d", &val);
+            if (n < 1)
+                printf("Usage: r <number>\n");
+            else
+                remove(val);
+            break;
+        case 'p':
+            print_heap();
+            break;
+        case 'q':
+            printf("Bye.\n");
+            exit(0);
+        case '\n':
+        case ' ':
+            break;
+        default:
+            printf("Unknown: %c\n", cmd);
+            break;
+        }
+        while ((n = getchar()) != '\n' && n != EOF)
+            ;
+    }
+}
